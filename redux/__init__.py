186d9e8c6b6547ed4d026d91cc1e81cccc6cf108
Sassan Haradji <sassanh@gmail.com>
2023-11-12 14:45:53 +0400

    feat: implement combine_reducers

--- redux/__init__.py
+++ redux/__init__.py
@@ -1,6 +1,7 @@
 # ruff: noqa: D101, D102, D103, D104, D107
 from __future__ import annotations
 
+import uuid
 from dataclasses import dataclass
 from inspect import signature
 from typing import (
@@ -133,3 +134,81 @@ def create_store(
         subscribe=subscribe,
         autorun=autorun,
     )
+
+
+@dataclass
+class CombineReducerActionBase(BaseAction):
+    _id: str
+
+
+@dataclass
+class CombineReducerRegisterActionPayload:
+    key: str
+    reducer: ReducerType
+
+
+@dataclass
+class CombineReducerRegisterAction(CombineReducerActionBase):
+    payload: CombineReducerRegisterActionPayload
+    type: Literal['REGISTER'] = 'REGISTER'
+
+
+@dataclass
+class CombineReducerUnregisterActionPayload:
+    key: str
+
+
+@dataclass
+class CombineReducerUnregisterAction(CombineReducerActionBase):
+    payload: CombineReducerUnregisterActionPayload
+    type: Literal['UNREGISTER'] = 'UNREGISTER'
+
+
+CombineReducerAction = CombineReducerRegisterAction | CombineReducerUnregisterAction
+
+
+def combine_reducers(
+    **reducers: ReducerType,
+) -> tuple[ReducerType, str]:
+    _id = uuid.uuid4().hex
+
+    def combined_reducer(
+        state: State | None,
+        action: CombineReducerAction,
+    ) -> State:
+        if action.type == 'REGISTER' and action._id == _id:
+            key = action.payload.key
+            reducer = action.payload.reducer
+            reducers[key] = reducer
+            state = cast(
+                State,
+                {
+                    **(state or {}),
+                    key: reducer(
+                        None,
+                        InitAction(type='INIT'),
+                    ),
+                },
+            )
+        if action.type == 'UNREGISTER' and action._id == _id:
+            key = action.payload.key
+            del reducers[key]
+            state = cast(
+                State,
+                {key_: state[key_] for key_ in reducers if key_ != key}
+                if state is not None
+                else {},
+            )
+
+        return cast(
+            State,
+            dict(
+                _id=_id,
+                **{
+                    key: reducer(None if state is None else state.get(key), action)
+                    for key, reducer in reducers.items()
+                },
+            ),
+        )
+
+    return (combined_reducer, _id)
